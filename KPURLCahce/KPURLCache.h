6f819a8232636fb5b3b6e916d365a62cdf02e4fd
NSBikep <dlutwanghaoyu@gmail.com>
2012-12-28 11:59:47 +0800

    add interface method in .h file

--- KPURLCahce/KPURLCache.h
+++ KPURLCahce/KPURLCache.h
@@ -13,6 +13,13 @@ typedef NS_ENUM(NSUInteger, EnumKPURLCachePolicy) {
     KPURLCachePolicyDisk = 4,       //Disk
 };
 
+typedef NS_ENUM(NSUInteger, EnumDateFormat) {
+	eDateFormatPNG,			//png
+	eDateFormatMP3,           //mp3
+    eDateFormatOther          //未记录的格式
+};
+
+
 @interface KPURLCache : NSObject{
     NSString            *_name;                  //name of cache
     NSString            *_plistPath;             //plist that records the disk Cache Info
@@ -44,6 +51,45 @@ typedef NS_ENUM(NSUInteger, EnumKPURLCachePolicy) {
 
 + (void)setSharedURLCache: (KPURLCache *)aCache;
 
+
+//store
+- (void)storeData:(NSData *)aData fileName:(NSString *)aName;
+
+- (void)storeData:(NSData *)aData fileName:(NSString *)aName version:(NSInteger)aVersion;
+
+- (void)storeData:(NSData *)aData fileName:(NSString *)aName format:(EnumDateFormat)aFormat;
+
+- (void)storeData:(NSData *)aData fileName:(NSString *)aName version:(NSInteger)aVersion format:(EnumDateFormat )aFormat;
+
+//read
+- (id)dataForFileName:(NSString *)aName;
+
+- (id)dataForFileName:(NSString *)aName version:(NSInteger)aVersion;
+
+- (id)dataForFileName:(NSString *)aName format:(EnumDateFormat)aFormat;
+
+- (id)dataForFileName:(NSString *)aName version:(NSInteger)aVersion format:(EnumDateFormat)aFormat;
+
+//find
+- (BOOL)hasDataForName:(NSString *)aName;
+
+- (BOOL)hasDataForName:(NSString *)aName version:(NSInteger)aVersion;
+
+- (BOOL)hasDataForName:(NSString *)aName format:(EnumDateFormat)aFormat;
+
+- (BOOL)hasDataForName:(NSString *)aName version:(NSInteger)aVersion format:(EnumDateFormat)aFormat;
+
+
+//remove
+- (void)removeAll:(BOOL)fromDisk;
+
+- (BOOL)removeFileName:(NSString *)aName fromDisk:(BOOL)fromDisk;
+// if needs we can add Others;
+
+//modify
+- (BOOL)moveDataFromName:(NSString *)anOldName toName:(NSString *)aNewName;
+
+- (void)modifyDataVersion:(NSInteger)aVersion forName:(NSString *)aName;
 
 
 @end
